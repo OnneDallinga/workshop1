ea4ed0d9752eb64010a61cd1393de37ea3c11adf
= <=>
2017-12-12 13:32:47 +0100

    Removed useless variables.

--- src/main/java/com/rsvier/workshop1/controller/Main.java
+++ src/main/java/com/rsvier/workshop1/controller/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import com.rsvier.workshop1.view.LoginMenuView;
 
 public class Main {
-	private int userMenuChoice;
-	private boolean isAdmin = false;
-	private String username;
 	
 	public static void main (String args[]) throws IOException {		
 		Controller Controller = new Controller(new LoginMenuView());
