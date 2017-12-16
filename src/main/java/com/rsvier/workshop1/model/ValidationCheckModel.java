81f7df1333e334e10cb491474da6e5d0c457288f
= <=>
2017-12-16 15:06:52 +0100

    Added dependency for mysql & added a connection for the login

--- src/main/java/com/rsvier/workshop1/model/ValidationCheckModel.java
+++ src/main/java/com/rsvier/workshop1/model/ValidationCheckModel.java
@@ -1,10 +1,28 @@
 package com.rsvier.workshop1.model;
 
+import java.sql.*;
+
 public class ValidationCheckModel {
 
 	public boolean validateUser(String username, String password) {
-		// TODO: Add code to connect to the database
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Load the driver
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "Onne", "Once!UponAT1me");
+			// Connect to the database
+			CallableStatement callableStatement = connection.prepareCall("{? = call correctLogin(?, ?)}");
+			callableStatement.setString(2,  username);
+			callableStatement.setString(3,  password);
+			callableStatement.registerOutParameter(1,  Types.INTEGER);
+			callableStatement.execute();
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return true;
 	}
-
 }
