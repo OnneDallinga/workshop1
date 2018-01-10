a1872ecd508ecb50afb30573a7f261349546cc1c
Geert Lamers <wickeh@gmail.com>
2018-01-10 11:05:45 +0100

    updated connector, moved xml reader

--- src/main/java/com/rsvier/workshop1/database/Connector.java
+++ src/main/java/com/rsvier/workshop1/database/Connector.java
@@ -5,22 +5,23 @@ import java.sql.*;
 public class Connector {
 
 	private Connection connection = null;
+	private String dbDriver = "com.mysql.jdbc.Driver";
+	private String dbUrl = "jdbc:mysql://localhost/mydb";
+	private String dbUser = "GeertL";
+	private String dbPass = "k44sBl0kJe$";
 	
 	public Connection getConnection() {
 		if (connection == null) {
 			try {
-				Class.forName("com.mysql.jdbc.Driver");
+				Class.forName(dbDriver);
 			} catch (ClassNotFoundException ex) {
-				System.out.println("Could not load driver");
+				System.out.println("Error: could not load driver");
 				System.exit(1);
 			}
 			try {
-				connection = DriverManager.getConnection(
-								"jdbc:mysql://localhost/mydb",
-								"Battlehenkie",
-								"koekjes");
+				connection = DriverManager.getConnection( dbUrl, dbUser, dbPass);
 			} catch (SQLException ex){
-				System.out.println("Invalid url, user and/or password");
+				System.out.println("Invalid: check url, user, password");
 				System.exit(1);
 			}
 		}
--- src/main/java/com/rsvier/workshop1/database/XMLReader.java
+++ src/main/java/com/rsvier/workshop1/database/XMLReader.java
@@ -1,16 +1,16 @@
-package com.rsvier.workshop1.model;
+package com.rsvier.workshop1.database;
 
-import java.net.URL;
+import java.io.File;
+import java.io.IOException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 
 public class XMLReader {
 
-    public Document parse(URL url) throws DocumentException {
+    public Document parse(File file) throws DocumentException {
         SAXReader reader = new SAXReader();
-        Document document;
-		document = reader.read(url);
+        Document document = reader.read(file);
         return document;
     }
 }
