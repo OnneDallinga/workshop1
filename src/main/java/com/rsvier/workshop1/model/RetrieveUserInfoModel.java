4d5efd154d5d0e1d4ae0a7914b671c68953b8607
= <=>
2018-01-14 16:39:51 +0100

    Fixed Hikari dependency

--- src/main/java/com/rsvier/workshop1/model/RetrieveUserInfoModel.java
+++ src/main/java/com/rsvier/workshop1/model/RetrieveUserInfoModel.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.*;
 import java.util.*;
+import com.zaxxer.hikari.*;
 
 public class RetrieveUserInfoModel extends Model {
 	
