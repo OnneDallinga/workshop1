6c1c912a38f51ec268343f93f3d51989571ce44b
Geert Lamers <wickeh@gmail.com>
2018-01-17 03:17:10 +0100

    fixed faulty merge

--- src/main/java/com/rsvier/workshop1/model/dao/CustomerDAOImpl.java
+++ src/main/java/com/rsvier/workshop1/model/dao/CustomerDAOImpl.java
@@ -5,46 +5,8 @@ import java.util.ArrayList;
 import com.rsvier.workshop1.model.Customer;
 import com.rsvier.workshop1.database.DataSource;
 
-import com.rsvier.workshop1.model.Customer;
-
 public class CustomerDAOImpl implements CustomerDAO {
 
-	@Override
-	public void createCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public ArrayList<Customer> findAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Customer findCustomer(long customerId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void updateCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteCustomer(long customerId) {
-		// TODO Auto-generated method stub
-		
-	}
-		
 	private Connection connection;
 	private String query;
 	private PreparedStatement statement;
