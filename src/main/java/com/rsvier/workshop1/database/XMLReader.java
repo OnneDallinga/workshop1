67d0cc8ba2349b5948ea4e5a3c27c5edf1c04cfd
Geert Lamers <wickeh@gmail.com>
2018-02-01 11:52:18 +0100

    added XML Reader

--- src/main/java/com/rsvier/workshop1/database/XMLReader.java
+++ src/main/java/com/rsvier/workshop1/database/XMLReader.java
@@ -1,16 +1,54 @@
 package com.rsvier.workshop1.database;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.io.SAXReader;
+import org.dom4j.Element;
+import org.dom4j.Node;
 
 public class XMLReader {
 
-    public Document parse(File file) throws DocumentException {
-        SAXReader reader = new SAXReader();
-        Document document = reader.read(file);
-        return document;
-    }
+	Document document = null;
+	Element rootElement = null;
+
+	public XMLReader() {
+		DocumentBuilderFactory builderFactory =
+				DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+			builder = builderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();  
+		}
+		try {
+			document = (Document) builder.parse(
+					new FileInputStream("dbproperties.xml"));
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		rootElement = document.getRootElement();
+	}
+
+	public String getUrl() {
+		Node urlNode = rootElement.selectSingleNode("url");
+		return urlNode.getText();
+	}
+
+	public String getUsername() {
+		Node usernameNode = rootElement.selectSingleNode("username");
+		return usernameNode.getText();
+	}
+
+	public String getPassword() {
+		Node passwordNode = rootElement.selectSingleNode("password");
+		return passwordNode.getText();
+	}
 }
