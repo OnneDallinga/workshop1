353cb60d8d892ac79b17a9d0a4f3c2c1b3231980
Geert Lamers <wickeh@gmail.com>
2018-02-22 16:44:12 +0800

    some QOL improvements to order DAO impl; added test

--- src/main/java/com/rsvier/workshop1/model/dao/OrderDAOImpl.java
+++ src/main/java/com/rsvier/workshop1/model/dao/OrderDAOImpl.java
@@ -19,19 +19,18 @@ public class OrderDAOImpl implements OrderDAO {
 		int newOrderId = 0;
 		query = "INSERT INTO order (total_price, total_products, shipped_status, customerID) VALUES (?,?,?,?);";
 		try (Connection conn = DataSource.getConnection();
-				PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-				ResultSet rs = stmt.getGeneratedKeys();) {
+				PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);) {
 			stmt.setBigDecimal(1, order.getOrderPriceTotal());
 			stmt.setInt(2, order.getOrderItemsTotal());
 			stmt.setBoolean(3,  order.isShipped());
 			stmt.setInt(4, order.getCustomerId());
 			stmt.executeUpdate();
-			try {
+			try (ResultSet rs = stmt.getGeneratedKeys();){
 				if (rs.next()) {
 					newOrderId = rs.getInt(1);
 					order.setOrderId(newOrderId);
 					logger.info("Succesfully created new order.");
-				}           
+				}
 			} catch (SQLException e) {
 				System.out.println("Creating new order failed.");
 			}
@@ -46,20 +45,24 @@ public class OrderDAOImpl implements OrderDAO {
 		List<Order> list = new ArrayList<Order>();
 		query = "SELECT * FROM order;";
 		try (Connection conn = DataSource.getConnection();
-				PreparedStatement stmt = conn.prepareStatement(query);
-				ResultSet rs = stmt.executeQuery();) {
-			logger.info("Connected to database.");    
-			while(rs.next()) {
-				Order order = new Order();
-				order.setOrderId(rs.getInt(1));
-				order.setOrderPriceTotal(rs.getBigDecimal(2));
-				order.setOrderItemsTotal(rs.getInt(3));
-				order.setShipped(rs.getBoolean(4));
-				order.setCustomerId(rs.getInt(5));
-				list.add(order);
+				PreparedStatement stmt = conn.prepareStatement(query);) {
+			logger.info("Connected to database.");
+			try (ResultSet rs = stmt.executeQuery();) {
+				while(rs.next()) {
+					Order order = new Order();
+					order.setOrderId(rs.getInt(1));
+					order.setOrderPriceTotal(rs.getBigDecimal(2));
+					order.setOrderItemsTotal(rs.getInt(3));
+					order.setShipped(rs.getBoolean(4));
+					order.setCustomerId(rs.getInt(5));
+					list.add(order);
+				}
+				logger.info("Total orders:" + rs.getRow());
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			logger.info("Total orders:" + rs.getRow());
 		} catch (SQLException e) {
+			logger.info("Could not retrieve orders.");
 			e.printStackTrace();
 		} 
 		return list;
@@ -70,16 +73,19 @@ public class OrderDAOImpl implements OrderDAO {
 		Order foundOrder = new Order();
 		query = "SELECT * FROM order WHERE id=?";
 		try (Connection conn = DataSource.getConnection();
-				PreparedStatement stmt = conn.prepareStatement(query);
-				ResultSet rs = stmt.executeQuery();) {
+				PreparedStatement stmt = conn.prepareStatement(query);) {
 			logger.info("Connected to database.");
-			stmt.setObject(1, orderId);	      
-			if(rs.next()) {
-				foundOrder.setOrderId(rs.getInt(1));
-				foundOrder.setOrderPriceTotal(rs.getBigDecimal(2));
-				foundOrder.setOrderItemsTotal(rs.getInt(3));
-				foundOrder.setShipped(rs.getBoolean(4));
-				foundOrder.setCustomerId(rs.getInt(5));
+			stmt.setInt(1, orderId);
+			try (ResultSet rs = stmt.executeQuery();) {
+				if(rs.next()) {
+					foundOrder.setOrderId(rs.getInt(1));
+					foundOrder.setOrderPriceTotal(rs.getBigDecimal(2));
+					foundOrder.setOrderItemsTotal(rs.getInt(3));
+					foundOrder.setShipped(rs.getBoolean(4));
+					foundOrder.setCustomerId(rs.getInt(5));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
