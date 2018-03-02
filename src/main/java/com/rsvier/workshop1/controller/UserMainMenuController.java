4d65cb19c16008201b7d8b68827fed6f66b41f62
Geert Lamers <wickeh@gmail.com>
2018-03-02 10:25:27 +0100

    now actually fixed linking in user main menu controller..

--- src/main/java/com/rsvier/workshop1/controller/UserMainMenuController.java
+++ src/main/java/com/rsvier/workshop1/controller/UserMainMenuController.java
@@ -13,10 +13,9 @@ public class UserMainMenuController extends Controller {
 	@Override
 	public void runView() {
 		menuOptions = new HashMap<Integer, Controller>();
-		menuOptions.put(1, new UserOrderController(new UserOrderOverview()));
-		menuOptions.put(2, new CustomerController(new CustomerView()));
-		menuOptions.put(3, new ProductController(new ProductView()));
-		menuOptions.put(4, new OrderController(new OrderView()));
+		menuOptions.put(1, new CustomerController(new CustomerView()));
+		menuOptions.put(2, new ProductController(new ProductView()));
+		menuOptions.put(3, new OrderController(new OrderView()));
 		
 		currentMenu.displayMessage();
 		nextController = menuOptions.get(currentMenu.asksUserForMenuChoice(menuOptions));
